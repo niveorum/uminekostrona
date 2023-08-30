578f7281a6f2467ee2b53d371dba2720ce5a04e5
Snowyy4K <shadowek285@gmail.com>
2023-08-30 21:47:34 +0200

    Update deploy_script.sh, template

--- deploy_script.sh
+++ deploy_script.sh
@@ -4,10 +4,10 @@
 REMOTE_USER="ubuntu"    # Replace with your remote user
 REMOTE_HOST="130.61.128.218"    # Replace with your remote host
 REMOTE_DIR="~/app"                # Remote directory where you want to deploy the app
-REPO_URL="https://github.com/Snowyy4K/uminekostrona.git"  # Replace with your repo URL
+REPO_URL="WKLEJ_TU_KLUCZ_GITHUB@github.com/Snowyy4K/uminekostrona.git"  # Replace with your repo URL
 
 # SSH into the remote instance, update or clone the repo, and perform deployment
-ssh $REMOTE_USER@$REMOTE_HOST << EOF
+ssh -i ~/.ssh/umineko.key $REMOTE_USER@$REMOTE_HOST << EOF
     cd $REMOTE_DIR
     if [ -d "app" ]; then
         cd app
