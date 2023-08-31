d3c6960896aa4fbb601f1f1697c88449fb075330
Ideefixze <kaczooorqu@gmail.com>
2023-08-31 20:29:34 +0200

    Typescript added

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css';
+
+interface AppProps {
+  
+}
+
+const App: React.FC<AppProps> = (props) => {
+  return (
+    <div>
+      Umineko
+    </div>
+  );
+};
+
+export default App;
