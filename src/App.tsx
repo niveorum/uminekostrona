270acb0367014a3434f0e404d3403e846f4a922f
Snowyy4K <shadowek285@gmail.com>
2023-09-01 20:55:15 +0200

    Cyce beatrice donice

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import './App.css';
+import DiscordLink from './components/DiscordLink';
 
 interface AppProps {
   
 }
 
+
 const App: React.FC<AppProps> = (props) => {
   return (
     <div>
       Umineko
+      <DiscordLink/>
     </div>
   );
 };
---
+++ src/components/DiscordLink.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const DiscordLink: React.FC = () => {
+  return (
+    <div>
+      <a href="https://discord.gg/wfF3zVJw9g" target="_blank" rel="noopener noreferrer">
+        <img src="cycebeatrice.jpg" alt="cyce beatrice hehe donice" />
+      </a>
+    </div>
+  );
+};
+
+export default DiscordLink;
