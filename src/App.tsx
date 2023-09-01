680f9d725e692d2b6bc23c2dd10eaecae05660cb
Snowyy4K <shadowek285@gmail.com>
2023-09-01 21:22:33 +0200

    Umineko.pl logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css';
 import DiscordLink from './components/DiscordLink';
+import Uminekopeel from './components/Uminekopeel';
 
 interface AppProps {
   
@@ -9,7 +10,10 @@ interface AppProps {
 const App: React.FC<AppProps> = (props) => {
   return (
     <div>
+      <a href="https://discord.gg/wfF3zVJw9g" target="_blank" rel="noopener noreferrer">
       Umineko
+      </a>
+      <Uminekopeel/>
       <DiscordLink/>
     </div>
   );
