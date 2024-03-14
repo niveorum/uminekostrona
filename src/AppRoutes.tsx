8f522c98d8850ae85c5e1b773984c9256dd0d437
Snowyy4K <shadowek285@gmail.com>
2024-03-14 09:38:41 +0100

    fix 3

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -12,7 +12,7 @@ export const AppRoutes: FC = () => {
   return (
     <div>
     <Navbar/>
-      <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <BrowserRouter>
         <Routes>
           <Route path='/' element={<Home/>}>         
             <Route path='' element={
